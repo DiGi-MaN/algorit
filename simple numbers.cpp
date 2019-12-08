a749ed0c6e2a15d62d675f24e868b432a6e42282
shinonhorror <57846361+shinonhorror@users.noreply.github.com>
2019-12-08 19:40:57 +0300

    Add files via upload

---
+++ simple numbers.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+class simple {
+	int n, y=0, i, j;
+public:
+	void input();
+	void test();
+	void output();
+};
+void simple::input() {
+	cout << "Input first number "<< "x= ";
+	cin >> n;
+}
+void simple::output() {
+	cout << "simple number is " << n << endl;;
+}
+void simple::test() {
+	i = 2; j = 0;
+	while (y<10) {
+		while ((j != 1) && (i * i <= n)) {
+			if (n % i == 0) {
+				j = 1;
+				i = i++;
+			}
+			else {
+				i = i++;
+			}
+		}
+		if (j != 1) {
+			output();
+				n++;
+				y++;
+			}
+		else {
+			n++;
+		}
+		i = 2; j = 0;
+		}
+	}
+void main() {
+	simple A;
+	A.input();
+	A.test();
+}
